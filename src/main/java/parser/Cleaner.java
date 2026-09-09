51c1e8c6e8463138351f31f47ab973a30eb4e1d9
ggiovadam <guido.giovadam@gmail.com>
2026-09-10 01:53:59 +0200

    [feat] Closed utility class Cleaner

--- src/main/java/parser/Cleaner.java
+++ src/main/java/parser/Cleaner.java
@@ -3,7 +3,11 @@ package parser;
 import java.util.List;
 import java.util.function.Predicate;
 
-public class Cleaner {
+public final class Cleaner {
+    private Cleaner() {
+        throw new UnsupportedOperationException("Don't instantiate Cleaner");
+    }
+    
     public static List<Token> clean(List<Token> tokens) {
         return tokens.stream()
                      .filter(Predicate.not(Token::isWhitespace))
