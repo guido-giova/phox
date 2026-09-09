ada33dca9fe72cac36620d50e46e973dcc6fd5fc
ggiovadam <guido.giovadam@gmail.com>
2026-09-09 19:00:46 +0200

    [feat] Add Token class

---
+++ src/main/java/parser/Token.java
@@ -0,0 +1,12 @@
+package parser;
+
+public sealed interface Token {
+    
+    int start();
+    
+    record Word(int start, String str) implements Token {}
+    record NumberLiteral(int start, String str) implements Token {}
+    record Whitespace(int start, String str) implements Token {}
+    record StringLiteral(int start, String str) implements Token {}
+    record Comment(int start, String str) implements Token {}
+}
