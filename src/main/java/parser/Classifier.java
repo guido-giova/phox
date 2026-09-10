d5f2dd16f838f5010d65bbb9998b787d0d78b95e
ggiovadam <guido.giovadam@gmail.com>
2026-09-10 18:26:42 +0200

    [feat] Add functionality to Classifier

--- src/main/java/parser/Classifier.java
+++ src/main/java/parser/Classifier.java
@@ -1,8 +1,46 @@
 package parser;
 
+import java.io.File;
+
 public final class Classifier {
     private Classifier() {
         throw new UnsupportedOperationException("Don't instantiate Classifier");
     }
     
+    public static void compilePath(String[] path) {
+        Classifier.compilePath(String.join(" ", path));
+    }
+    
+    private static void compilePath(String path) {
+        File main = new File(path);
+        if (! main.exists()) {
+            System.err.println("File not exists");
+            System.exit(1);
+        }
+        Classifier.checkFileType(main, "");
+    }
+    
+    private static void checkFileType(File file, String packageName) {
+        if (file.isFile() && file.getName().endsWith(".phox")) {
+            Classifier.handleFile(file, packageName);
+        }
+        if (file.isDirectory()) {
+            File[] children = file.listFiles();
+            if (children == null) { return; }
+            
+            for (File child : children) {
+                Classifier.checkFileType(child, Classifier.getPackageName(file, packageName));
+            }
+        }
+    }
+    
+    private static String getPackageName(File file, String packageName) {
+        String newPackageName = packageName;
+        if (!packageName.isEmpty()) {newPackageName += ".";}
+        return newPackageName + file.getName();
+    }
+    
+    private static void handleFile(File file, String packageName) {
+        System.out.println(packageName + "." + file.getName());
+    }
 }
