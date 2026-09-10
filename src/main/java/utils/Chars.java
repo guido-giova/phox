60b777ab12cfc706d93412940ef8c39136efba8e
ggiovadam <guido.giovadam@gmail.com>
2026-09-10 16:51:07 +0200

    [feat] Add checks for binary digits, octal digits, and hex digits to Chars

--- src/main/java/utils/Chars.java
+++ src/main/java/utils/Chars.java
@@ -3,12 +3,24 @@ package utils;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Chars {
+    public static boolean isIdentifierChar(char c) {
+        return Character.isLetterOrDigit(c) || c == '_';
+    }
+    
+    public static boolean isBinDigit(char c) {
+        return c == '0' || c == '1';
+    }
+    
+    public static boolean isOctDigit(char c) {
+        return c >= '0' && c <= '7';
+    }
+    
     public static boolean isDecDigit(char c) {
         return c >= '0' && c <= '9' || c == '_';
     }
     
-    public static boolean isIdentifierChar(char c) {
-        return Character.isLetterOrDigit(c) || c == '_';
+    public static boolean isHexDigit(char c) {
+        return Chars.isDecDigit(c) || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
     }
     
     public static boolean isExponentSymbol(char c) {
