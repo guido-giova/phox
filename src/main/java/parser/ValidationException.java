ba2cbfd0d74754c9cdee8e31dde9b574263edd96
ggiovadam <guido.giovadam@gmail.com>
2026-09-10 03:02:35 +0200

    [chore] Add Javadoc to ValidationException

--- src/main/java/parser/ValidationException.java
+++ src/main/java/parser/ValidationException.java
@@ -1,10 +1,25 @@
 package parser;
 
+/**
+ * Exception to indicate an error in validation of a Phox class
+ */
 public class ValidationException extends RuntimeException {
+    /**
+     * Constructs a new validation exception with the given message.
+     *
+     * @param message the reason
+     */
     public ValidationException(String message) {
         super(message);
     }
     
+    /**
+     * Constructs a new validation exception with the given message and the given index, with the format:
+     * "[<b>message</b>] at [<b>at</b>]"
+     *
+     * @param message the reason
+     * @param at      the index of the error
+     */
     public ValidationException(String message, int at) {
         this(String.format("%s at %d", message, at));
     }
