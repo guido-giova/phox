4c84e0c553e040e6fb33b3ddd84b06002c7f4f2f
ggiovadam <guido.giovadam@gmail.com>
2026-09-10 02:49:11 +0200

    [chore] Add Javadoc to Compiler

--- src/main/java/parser/Compiler.java
+++ src/main/java/parser/Compiler.java
@@ -2,11 +2,21 @@ package parser;
 
 import java.util.List;
 
+/**
+ * Utility class for determining if a String is a valid program in the Phox language.
+ */
 public final class Compiler {
     private Compiler() {
         throw new UnsupportedOperationException("Don't instantiate Compiler");
     }
     
+    /**
+     * Validates if the given String is valid.
+     *
+     * @param text that will be validated
+     * @return Failure with reason if it's invalid or Success with tokens if valid.
+     * @see CompilerResponse
+     */
     public static CompilerResponse validate(String text) {
         try {
             List<Token> tokens;
