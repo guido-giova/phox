412b357befc545e1907a1e9e2ba7ea9aad59b88f
ggiovadam <guido.giovadam@gmail.com>
2026-09-17 21:57:05 +0200

    [feat] Add MethodDeclarationNode

--- src/main/java/parser/ast/ASTNode.java
+++ src/main/java/parser/ast/ASTNode.java
@@ -2,6 +2,8 @@ package parser.ast;
 
 import parser.Token;
 
+import java.util.List;
+
 public sealed interface ASTNode {
     record FieldDeclarationNode(
         String name,
@@ -11,4 +13,19 @@ public sealed interface ASTNode {
         Token.DynamismTypeKind dynamism,
         Token definition
     ) implements ASTNode {}
+    
+    record MethodDeclarationNode(
+            Token.VisibilityTypeKind visibility,
+            Token.DynamismTypeKind dynamism,
+            Token.InheritanceTypeKind inheritance,
+            TypeReference type,
+            String name,
+            List<Parameter> params,
+            MethodBody body
+    ) implements ASTNode {}
+    
+    sealed interface MethodBody {
+        record Unparsed(List<Token> tokens) implements MethodBody {}
+        record Parsed(String block) implements MethodBody {} // todo: create statement block
+    }
 }
