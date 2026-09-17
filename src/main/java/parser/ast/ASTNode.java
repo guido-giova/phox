aa3f56042edc4a2ae9958fec65dc54ad0ed6f999
ggiovadam <guido.giovadam@gmail.com>
2026-09-17 22:16:33 +0200

    [feat] Add TypeNode.ClassDeclarationNode for classes

--- src/main/java/parser/ast/ASTNode.java
+++ src/main/java/parser/ast/ASTNode.java
@@ -28,4 +28,17 @@ public sealed interface ASTNode {
         record Unparsed(List<Token> tokens) implements MethodBody {}
         record Parsed(String block) implements MethodBody {} // todo: create statement block
     }
+    
+    sealed interface TypeNode extends ASTNode {
+        record ClassDeclarationNode(
+                String name,
+                List<String> generics,
+                Token.VisibilityTypeKind visibility,
+                Token.DynamismTypeKind dynamism,
+                Token.InheritanceTypeKind inheritance,
+                List<FieldDeclarationNode> fields,
+                List<MethodDeclarationNode> methods,
+                List<TypeNode> childTypes
+        ) implements TypeNode {}
+    }
 }
