591f5c2c4fe07762fc7160359327b130e0ced7ad
ggiovadam <guido.giovadam@gmail.com>
2026-09-17 21:52:14 +0200

    [feat] Add Pointer to TypeReference

--- src/main/java/parser/ast/TypeReference.java
+++ src/main/java/parser/ast/TypeReference.java
@@ -7,4 +7,5 @@ import java.util.List;
 sealed interface TypeReference {
     record Primitive(Token.DataTypeKind kind) implements TypeReference {}
     record Named(Token.Word name, List<TypeReference> generics) implements TypeReference {}
+    record Pointer(TypeReference baseType) implements TypeReference {}
 }
