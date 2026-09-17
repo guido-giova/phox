b15bf8969782e2e1754f76b3c5dc8b23edf312af
ggiovadam <guido.giovadam@gmail.com>
2026-09-17 19:42:05 +0200

    [feat] Add FieldDeclarationNode for defining fields

---
+++ src/main/java/parser/ASTNode.java
@@ -0,0 +1,11 @@
+package parser;
+
+import java.util.List;
+
+public sealed interface ASTNode {
+    record FieldDeclarationNode(
+        String name,
+        TypeReference type,
+        List<Token.ModifierTypeKind> modifiers
+    ) implements ASTNode {}
+}
