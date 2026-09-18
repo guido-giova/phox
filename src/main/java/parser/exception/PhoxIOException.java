b5d54cbe25dbeafe697628b2f1ae4047dca70245
ggiovadam <guido.giovadam@gmail.com>
2026-09-19 00:47:55 +0200

    [feat] Add PhoxIOException

---
+++ src/main/java/parser/exception/PhoxIOException.java
@@ -0,0 +1,20 @@
+package parser.exception;
+
+import java.io.Serial;
+
+/**
+ * Runtime exception (unchecked) for when general IO failures or interruptions.
+ */
+public class PhoxIOException extends PhoxCompilationException {
+  @Serial
+  private static final long serialVersionUID = -405780014323945983L;
+  
+  /**
+   * Constructs an io exception with the given message.
+   *
+   * @param message the reason for the exception
+   */
+  public PhoxIOException(String message) {
+        super(message);
+    }
+}
