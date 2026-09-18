84e22945391eea5c3480d0b8a123dd166d26b6c2
ggiovadam <guido.giovadam@gmail.com>
2026-09-19 00:34:37 +0200

    [feat] Actually proper facade

--- src/main/java/parser/Phox.java
+++ src/main/java/parser/Phox.java
@@ -1,40 +1,45 @@
 package parser;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.List;
 
-public final class Classifier {
-    private Classifier() {
-        throw new UnsupportedOperationException("Don't instantiate Classifier");
+public final class Phox {
+    private Phox() {
+        throw new UnsupportedOperationException("Don't instantiate Phox");
     }
     
-    public static void compilePath(String[] path) {
-        Classifier.compilePath(String.join(" ", path));
+    public static void compile(String[] path) {
+        Phox.compile(String.join(" ", path));
     }
     
-    private static void compilePath(String path) {
+    private static void compile(String path) {
         File main = new File(path);
         if (! main.exists()) {
             System.err.println("File not exists");
             System.exit(1);
         }
-        Classifier.checkFileType(main, "");
+        Phox.checkFileType(main, "");
     }
     
     private static void checkFileType(File file, String packageName) {
         if (file.isFile() && file.getName().endsWith(".phox")) {
-            Classifier.handleFile(file, packageName);
+            Phox.handleFile(file, packageName);
         }
         if (file.isDirectory()) {
-            File[] children = file.listFiles();
-            if (children == null) { return; }
-            
-            for (File child : children) {
-                Classifier.checkFileType(child, Classifier.getPackageName(file, packageName));
-            }
+            Phox.handleDirectory(file, packageName);
+        }
+    }
+    
+    private static void handleDirectory(File file, String packageName) {
+        File[] children = file.listFiles();
+        if (children == null) {return;}
+        
+        for (File child : children) {
+            Phox.checkFileType(child, Phox.getPackageName(file, packageName));
         }
     }
     
@@ -48,7 +53,7 @@ public final class Classifier {
         final String fileName = file.getName();
         final String completeClassName = packageName + "." + fileName.substring(0, fileName.length() - ".phox".length());
         
-        String fileText = Classifier.readFile(file);
+        String fileText = Phox.readFile(file);
         
         CompilerResponse response = Compiler.validate(fileText);
         switch (response) {
