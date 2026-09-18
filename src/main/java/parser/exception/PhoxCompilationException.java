de1ef9482c5e3c81ed4a4208d1633467cba2d468
ggiovadam <guido.giovadam@gmail.com>
2026-09-19 00:58:02 +0200

    [chore] Remove redundant "error" text from PhoxCompilationException's Javadoc

--- src/main/java/parser/exception/PhoxCompilationException.java
+++ src/main/java/parser/exception/PhoxCompilationException.java
@@ -10,7 +10,7 @@ public class PhoxCompilationException extends RuntimeException {
     private static final long serialVersionUID = 3452621655957814386L;
     
     /**
-     * Constructs a compilation exception error with the given message.
+     * Constructs a compilation exception with the given message.
      *
      * @param message the reason for the exception
      */
