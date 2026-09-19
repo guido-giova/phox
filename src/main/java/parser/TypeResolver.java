cf1c9b8229fa2bdb11fc9a9354c63f9a606640c5
ggiovadam <guido.giovadam@gmail.com>
2026-09-19 03:49:22 +0200

    [feat] Add constants for default imported package and default parent class in TypeResolver

--- src/main/java/parser/TypeResolver.java
+++ src/main/java/parser/TypeResolver.java
@@ -3,6 +3,9 @@ package parser;
 import java.util.List;
 
 public final class TypeResolver {
+    private static final String DEFAULT_IMPORTED_PACKAGE = "phox.main";
+    private static final String DEFAULT_PARENT_CLASS = "Object";
+    
     public static void resolve(List<TreeDefinition> node) {
     
     }
