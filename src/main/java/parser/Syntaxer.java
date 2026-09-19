e25d41901c83a85fe3a593dba651731d4f831b75
ggiovadam <guido.giovadam@gmail.com>
2026-09-19 03:20:50 +0200

    [chore] Add Javadoc to Syntaxer

--- src/main/java/parser/Syntaxer.java
+++ src/main/java/parser/Syntaxer.java
@@ -4,11 +4,20 @@ import parser.ast.ASTNode;
 
 import java.util.List;
 
+/**
+ * Utility class that accepts a list of tokens and outputs an Abstract Syntax Tree of the file.
+ */
 public final class Syntaxer {
     private Syntaxer() {
         throw new UnsupportedOperationException("Don't instantiate Syntaxer");
     }
     
+    /**
+     * Given a list of tokens, interpretes the syntax and creates an Abstract Syntax Tree
+     *
+     * @param tokens that will be resolved
+     * @return the parent node
+     */
     public static ASTNode resolve(List<Token> tokens) {
         
         return null;
