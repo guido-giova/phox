82ef5ab0d9254039d129677ef8981292f23efcda
ggiovadam <guido.giovadam@gmail.com>
2026-09-19 03:14:21 +0200

    [feat] Add outershell for Syntaxer

--- src/main/java/parser/Syntaxer.java
+++ src/main/java/parser/Syntaxer.java
@@ -1,9 +1,16 @@
 package parser;
 
+import parser.ast.ASTNode;
+
+import java.util.List;
+
 public final class Syntaxer {
     private Syntaxer() {
         throw new UnsupportedOperationException("Don't instantiate Syntaxer");
     }
     
-    
+    public static ASTNode resolve(List<Token> tokens) {
+        
+        return null;
+    }
 }
