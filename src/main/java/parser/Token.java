3a42542e211dc5fb58acc5b0a8543e2f9e481104
ggiovadam <guido.giovadam@gmail.com>
2026-09-19 04:11:22 +0200

    [feat] Add CharacterLiteral to Token

--- src/main/java/parser/Token.java
+++ src/main/java/parser/Token.java
@@ -15,6 +15,7 @@ public sealed interface Token {
     record NumberLiteral(int start, String str) implements Token {}
     record Whitespace(int start, String str) implements Token {}
     record StringLiteral(int start, String str) implements Token {}
+    record CharacterLiteral(int start, char value) implements Token {}
     record Comment(int start, String str) implements Token {}
     record Keyword(int start, KeywordKind str) implements Token {}
     record Symbol(int start, SymbolKind kind) implements Token {
